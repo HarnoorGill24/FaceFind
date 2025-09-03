8232b6d64c66f0bd39d796051bab5ebbc6f94ae3
HarnoorGill24 <gill_harnoor@hotmail.com>
2025-09-02 20:24:17 -0700

    Strengthen label sanitization

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -23,3 +23,21 @@ def test_sanitize_label_strip_non_alnum():
     assert sanitize_label("foo!bar#baz", replacement=None) == "foobarbaz"
     # All stripped -> unknown
     assert sanitize_label("!!!", replacement=None) == "unknown"
+
+
+def test_sanitize_label_empty():
+    assert sanitize_label("") == "unknown"
+
+
+def test_sanitize_label_path_traversal():
+    assert sanitize_label("../secret") == "secret"
+
+
+def test_sanitize_label_special_chars_and_dots():
+    label = "foo.bar?baz"
+    assert sanitize_label(label) == "foo_bar_baz"
+
+
+def test_sanitize_label_max_length():
+    long_label = "a" * 150
+    assert len(sanitize_label(long_label)) == 100
